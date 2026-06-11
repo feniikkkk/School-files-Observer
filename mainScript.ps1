fab27fdf2c49cf571573272ab1e266ef482ea001
feniikkkk <richard.kunz18@gmail.com>
2026-06-11 20:35:10 +0200

    Finished origin check and added file collision handling, Overviewed the complete code and took care of any hazards. Updated read me with a "how to use" tutorial

--- mainScript.ps1
+++ mainScript.ps1
@@ -16,10 +16,11 @@ else {
 
         if ($dialog.ShowDialog() -eq 'OK') {
             if (Test-Path $dialog.SelectedPath) {
-                (Get-Content $PSCommandPath) -replace '\$SavedPath = "UNSET"', "`$SavedPath = `"$($dialog.SelectedPath)`"" | Set-Content $PSCommandPath
+                (Get-Content $PSCommandPath) -replace '\$personalPath = ''UNSET''', "`$personalPath = `"$($dialog.SelectedPath)`"" | Set-Content $PSCommandPath # **
                 $pathToSchoolFolder = $dialog.SelectedPath
+                break;
             }
-            [System.Windows.MessageBox]::Show("The selected path is invalid or does not exist.", "Path Error", "OK", "Error")
+            [System.Windows.MessageBox]::Show("The selected path is invalid or does not exist.", "Path Error", "OK", "Error") 
         }
         else {
             [System.Windows.MessageBox]::Show("A valid destination directory is required to proceed.", "Operation Canceled", "OK", "Warning")
@@ -38,7 +39,7 @@ function pathFinder($keyword) {
         }
     }
 
-    New-Item -Path $pathToSchoolFolder -ChildPath $keyword
+    (New-Item -Path $pathToSchoolFolder -Name $keyword -ItemType Directory).FullName
 
 }
 
@@ -53,33 +54,57 @@ $observer.EnableRaisingEvents = $true
 $observer.InternalBufferSize = 64KB  
 
 $action = {
-    
+    Start-Sleep -Seconds 2
     $path = $Event.SourceEventArgs.FullPath
     $currentFile = Get-Item -Path $path
-    origin = getOrigin($path)
-    if (origin.length -gt 0) {
+    if ($currentFile.Extension -match "crdownload|tmp") { return } 
+    $origin = getOrigin($path)
+    if ($origin.length -gt 0) {
 
-    } 
-    if ( $path -match 'M231' ) {
-        # TODO: Get Obsidian stuff into the correct Obsidian directory 
-    }
-    elseif ( $path -match 'M\d{3}' ) { # Searches for classic Module IT Style "M**" f.E M122 
-        #TODO: File collision handling 
-        Move-Item $currentFile $pathToSchoolFolder -ErrorAction Stop 
-        # TODO: Organization 
-    }
+        $target = pathFinder($origin)
+        $expectedPath = Join-Path $target $currentFile.Name
+        $baseTargetPath = $expectedPath
+
+        $counter = 1 
+        while (Test-Path $expectedPath) {                       # ** --> 
+            $folder = Split-Path $baseTargetPath -Parent
+            $name = (Get-Item $baseTargetPath).BaseName
+            $ext = (Get-Item $baseTargetPath).Extension
+            $expectedPath = "$folder\$name ($counter)$ext"
+            $counter++
+        }                                                       # ** <--
     
-    # TODO: More school-like checks.
-}
+        Move-Item $currentFile $expectedPath -ErrorAction Stop 
+        if ($origin -eq 'M231' -and $expectedPath -match '\.zip$') {
+            $targetFolder = Split-Path $expectedPath -Parent
+            Expand-Archive -Path $expectedPath -DestinationPath $targetFolder
+            Remove-Item -Path $expectedPath -Force
+        } 
+    }
+} 
+
 
 function getOrigin($path) {
+    $zoneData = Get-Content -Path $path -Stream Zone.Identifier -ErrorAction SilentlyContinue
+    $hostUrl = ($zoneData | Where-Object { $_ -match "^HostUrl="}) -replace "HostUrl=", ""
+    $urlData = ($hostUrl -split "/")
+    $moduleName = ""
+    foreach ($data in $urlData) {
+        if ( $data -match 'M\d{3}') {
+            $moduleName = [regex]::Match($data, "M\d{3}").Value
+            break
+        }
+    }
+    if ($moduleName -eq "") { return $moduleName} 
+    return $moduleName
     
 }
 
-# Register-ObjectEvent -InputObject $observer -EventName 'Created' -Action $action
 
-# TODO: If enough time, archive-system 
 
+Register-ObjectEvent -InputObject $observer -EventName 'Created' -Action $action
+
+# Any lines that are fully written by an Artificial Intelligence are marked with "**" 
 
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_regular_expressions?view=powershell-7.6
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-7.6
